cc9f2b1dacd7edf42f33885bfa0834c8478a9358
Harshavardhan Gowda <harsha7697@gmail.com>
2025-02-10 05:10:10 +0000

    valkey-cli installation script fixed

--- scripts/redis_cli_installation.sh
+++ scripts/redis_cli_installation.sh
@@ -1,7 +1,22 @@
-sudo yum -y install openssl-devel gcc
-wget https://github.com/valkey-io/valkey/archive/refs/tags/7.2.6.tar.gz
-tar xvzf valkey-7.2.6.tar.gz
-cd valkey-7.2.6
-make distclean
-make valkey-cli BUILD_TLS=yes
-sudo install -m 755 src/valkey-cli /usr/local/bin/
+#!/usr/bin/bash
+
+install_valkey() {
+    echo "beginning install of valkey"
+    sudo yum -y install openssl-devel gcc
+    wget https://github.com/valkey-io/valkey/archive/refs/tags/7.2.6.tar.gz
+    tar xvzf 7.2.6.tar.gz
+    cd valkey-7.2.6
+    make distclean
+    make valkey-cli BUILD_TLS=yes
+    sudo install -m 755 src/valkey-cli /usr/local/bin/
+    echo "valkey successfull installed in /usr/local/bin/"
+}
+
+which valkey-cli >/dev/null 2>&1
+
+if [ $? -eq 0 ]
+then
+    echo "valkey-cli is already installed"
+else
+    install_valkey
+fi
